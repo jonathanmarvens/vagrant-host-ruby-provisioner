9176ee1b59480d2986c3c42e408d93c61b2418f3
Jonathan Barronville <jonathan.barronville@jebbit.com>
2014-01-13 13:23:52 -0500

    Fixed `clean` directory issue.

--- lib/vagrant-host-ruby-provisioner/provisioner.rb
+++ lib/vagrant-host-ruby-provisioner/provisioner.rb
@@ -1,19 +1,27 @@
 module VagrantPlugins
   module HostRubyProvisioner
     class Provisioner < Vagrant.plugin '2', :provisioner
-      def _execute_code
+      def _execute_clean
+        @config.clean.call
+      end
+
+      def _execute_routine
         @config.routine.call
       end
 
       def cleanup
-        @config.clean.call
+        if @config.cwd
+          Dir.chdir @config.cwd, &(method :_execute_clean)
+        else
+          _execute_clean
+        end
       end
 
       def provision
         if @config.cwd
-          Dir.chdir @config.cwd, &(method :_execute_code)
+          Dir.chdir @config.cwd, &(method :_execute_routine)
         else
-          _execute_code
+          _execute_routine
         end
       end
     end
